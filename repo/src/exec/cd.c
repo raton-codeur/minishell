5536edfe07d347dbb1aee6e7251c1e12e9c90b90
Jason Teste <jteste@k1r2p6.42mulhouse.fr>
2024-07-02 16:52:33 +0200

    cd

--- repo/src/exec/cd.c
+++ repo/src/exec/cd.c
@@ -3,42 +3,58 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qhauuy <qhauuy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jteste <jteste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:28:09 by jteste            #+#    #+#             */
-/*   Updated: 2024/07/02 16:23:24 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/07/02 16:50:52 by jteste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-// void	change_directory(char *path)
-// {
-// 	if (chdir(path) != 0)
-// 		perror("cd");
-// }
+void	change_directory(char *path)
+{
+	if (chdir(path) != 0)
+		perror("cd");
+}
 
-// int	cd_(t_tree *tree, t_data *data, int in_parent)
-// {
-// 	char *new_path;
-// 	prepare_argv(tree, data);
-// 	if (data->argc > 2)
-// 	{
-// 		ft_putendl_fd("cd: too many arguments", 2);
-// 		return (1);
-// 	}
-// 	else if (data->argc == 1)
-// 	{
-// 		new_path = get_env("HOME", data->env);
-// 		if (!new_path)
-// 		{
-// 			ft_putendl_fd("cd: HOME not set", 2);
-// 			return (1);
-// 		}
-// 	}
-// 	else
-// 		new_path = data->cmd->argv[1];
-// 	if (chdir(new_path) != 0)
-// 		perror("cd :");
-	
-// }
+char	*analyse_new_path(char *new_path, t_data *data)
+{
+	char	*home;
+
+	home = get_env("HOME", data->envp);
+	if (home == NULL)
+	{
+		ft_putendl_fd("cd: HOME not set", 2);
+		return (NULL);
+	}
+	if ((new_path[0] == '~' && new_path[1] == '\0') || (data->cmd->argc == 1))
+		return (home);
+	else if (new_path[0] == '~' && new_path[1] == '/')
+		new_path = ft_strjoin(home, new_path + 1);
+	if (new_path == NULL)
+		error_exit(MALLOC, data);
+	else
+		new_path = data->cmd->argv[1];
+	return (new_path);
+}
+
+int	cd_(t_tree *tree, t_data *data, int in_parent)
+{
+	char	*new_path;
+
+	prepare_argv(tree, data);
+	if (data->cmd->argc > 2)
+	{
+		ft_putendl_fd("cd: too many arguments", 2);
+		return (1);
+	}
+	else
+		new_path = analyse_new_path(data->cmd->argv[1], data);
+	if (new_path == NULL)
+		return (1);
+	modify_key_value(&data->envp, "OLDPWD", get_env("PWD", data->envp));
+	change_directory(new_path);
+	modify_key_value(&data->envp, "PWD", getcwd(NULL, 0));
+	return (0);
+}
