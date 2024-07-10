ea0767f257f4c7118f72a8746541eebbf91f57fd
Quentin Hauuy <qhauuy@k1r2p7.42mulhouse.fr>
2024-07-10 15:14:54 +0200

    fix de cd

--- repo/src/execution/cd.c
+++ repo/src/execution/cd.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:28:09 by jteste            #+#    #+#             */
-/*   Updated: 2024/07/08 22:56:02 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/07/10 15:13:18 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,45 +62,52 @@ static void	change_directory(char *path, t_data *data)
 
 	oldpwd = getcwd(NULL, 0);
 	if (oldpwd == NULL)
-		return (error_exit(MALLOC, data));
+		return (free(path), error_exit(MALLOC, data));
 	if (chdir(path) != 0)
-		return (perror("cd"), free(oldpwd));
+		return (free(path), perror("cd"), free(oldpwd));
 	free(path);
 	path = NULL;
 	reset_value("OLDPWD", oldpwd, data);
 	pwd = getcwd(NULL, 0);
 	if (pwd == NULL)
-		return (free(oldpwd), error_exit(MALLOC, data));
+		return (error_exit(MALLOC, data));
 	reset_value("PWD", pwd, data);
 }
 
-static char	*analyse_new_path(char *new_path, t_data *data)
+static char	*get_new_path(char *arg, t_data *data)
 {
-	char	*home;
+	char	*result;
+	char	*tmp;
 
-	home = get_value(in_env("HOME", data));
-	if (home == NULL)
-		ft_putendl_fd("cd: HOME not set", 2);
-	if ((data->cmd->argc == 1) || (new_path[0] == '~' && new_path[1] == '\0'))
+	if ((data->cmd->argc == 1)
+		|| (arg[0] == '~' && arg[1] == '\0')
+		|| (arg[0] == '~' && arg[1] == '/'))
 	{
-		if (home == NULL)
-			ft_putendl_fd("cd: HOME not set", 2);
-		home = ft_strdup(home);
-		if (home == NULL)
+		result = get_value(in_env("HOME", data));
+		if (result == NULL)
+			return (ft_putendl_fd("cd: HOME not set", 2), NULL);
+		result = ft_strdup(result);
+		if (result == NULL)
 			error_exit(MALLOC, data);
-		return (home);
-	}
-	else if (new_path[0] == '~' && new_path[1] == '/')
-	{
-		if (home == NULL)
-			ft_putendl_fd("cd: HOME not set", 2);
-		new_path = ft_strjoin(home, new_path + 1);
-		if (new_path == NULL)
-			error_exit(MALLOC, data);
+		if ((data->cmd->argc == 1) || (arg[0] == '~' && arg[1] == '\0'))
+			return (result);
+		else
+		{
+			tmp = result;
+			result = ft_strjoin(result, arg + 1);
+			if (result == NULL)
+				return (free(tmp), error_exit(MALLOC, data), NULL);
+			free(tmp);
+			return (result);
+		}
 	}
 	else
-		new_path = data->cmd->argv[1];
-	return (new_path);
+	{
+		result = ft_strdup(data->cmd->argv[1]);
+		if (result == NULL)
+			error_exit(MALLOC, data);
+		return (result);
+	}
 }
 
 int	cd_(t_tree *tree, t_data *data, int in_parent)
@@ -113,15 +120,15 @@ int	cd_(t_tree *tree, t_data *data, int in_parent)
 		ft_putendl_fd("cd: too many arguments", 2);
 		return (end_builtin(in_parent, 1, data));
 	}
-	new_path = analyse_new_path(data->cmd->argv[1], data);
+	new_path = get_new_path(data->cmd->argv[1], data);
 	if (new_path == NULL)
 		return (end_builtin(in_parent, 1, data));
 	if (check_cd_error(new_path) == 1)
-		return (end_builtin(in_parent, 1, data));
+		return (free(new_path), end_builtin(in_parent, 1, data));
 	if (check_symbolic_link(new_path) == 1)
 	{
 		ft_putendl_fd("cd: symbolic link not allowed", 2);
-		return (end_builtin(in_parent, 1, data));
+		return (free(new_path), end_builtin(in_parent, 1, data));
 	}
 	change_directory(new_path, data);
 	return (end_builtin(in_parent, 0, data));
